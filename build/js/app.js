d51d07e0ecfbfc6b5480e021b95b809c42b51c55
Daniel Correa <daniel.correa92@hotmail.com>
2022-02-19 03:36:55 -0600

    Probando cambios

--- build/js/app.js
+++ build/js/app.js
@@ -1,2 +1,2 @@
-document.addEventListener("DOMContentLoaded",()=>{iniciarApp()});const iniciarApp=()=>{};
+const expresiones={nombre:/^[a-zA-ZA\s]{1,40}$/,correo:/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,telefono:/^\d{10}$/};document.addEventListener("DOMContentLoaded",()=>{iniciarApp()});const iniciarApp=()=>{};
 //# sourceMappingURL=app.js.map
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,12 @@
+
+
+//expresiones regulares para validar
+const expresiones = {
+	nombre: /^[a-zA-ZA\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{10}$/ // 10 numeros
+}
+
 document.addEventListener('DOMContentLoaded',()=>{
 
     iniciarApp();
